92fb33cfca04a2405cdd1b223a8b916870cb423a
Aryan Singh <aryansin0448@k12.ipsd.org>
2023-09-06 18:06:31 -0500

    SignUp Auth Complete

--- client/src/app/api/auth/signup/page.tsx
+++ client/src/app/api/auth/signup/page.tsx
@@ -1,6 +1,27 @@
+"use client"
 import React from 'react'
+import {createUser} from './serveractions'
 
 export default function SignUp() {
+    
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const target = event.target as typeof event.target & {
+      email: { value: string };
+      password: { value: string };
+      name: { value: string };
+    };
+    const email = target.email.value;
+    const password = target.password.value;
+    const name = target.name.value;
+    const result = await createUser(name,email,password);
+    if (result.status === 200) {
+      // Redirect to the sign in page
+      window.location.href = '/api/auth/signin';
+    } else {
+      alert(result.error);
+    }
+  }  
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -15,10 +36,10 @@ export default function SignUp() {
               </h1>
              
            
-              <form className="space-y-4 md:space-y-4" action="#">
+              <form className="space-y-4 md:space-y-4" onSubmit={handleSubmit} >
               <div>
                       <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-50 0" placeholder="xxxxxxxxxxx" required/>
+                      <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-50 0" placeholder="xxxxxxxxxxx" required/>
                   </div>
                   <div>
                       <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
@@ -39,7 +60,7 @@ export default function SignUp() {
                       </div>
                      
                   </div>
-                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign Up</button>
+                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"  >Sign Up</button>
                   
                  
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
---
+++ client/src/app/api/auth/signup/serveractions.tsx
@@ -0,0 +1,25 @@
+"use server"
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+export const createUser = async (username: string, email: string, password: string | Buffer) => {
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Save the user to the database
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+        // Add other fields as needed
+      },
+    });
+    return { status: 200};
+  } catch (error) {
+    return { status: 400, error: 'An error occurred while creating the user.' };
+  }
+};
