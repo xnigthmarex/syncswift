8edb0365ae5e1173f678df99f5759b28e55e6edb
Aryan Singh <135762633+xnigthmarex@users.noreply.github.com>
2023-09-06 16:08:49 -0500

    Initial commit

---
+++ client/src/app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,60 @@
+
+import NextAuth, { type NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt'
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Sign in',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'hello@example.com'
+        },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials) {
+       
+
+        return {
+          id: "1" + '',
+          email: "aryan@lol.com",
+          name: "noob",
+          randomKey: 'Hey cool'
+        }
+      }
+    })
+  ],
+  callbacks: {
+    session: ({ session, token }) => {
+      console.log('Session Callback', { session, token })
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          randomKey: token.randomKey
+        }
+      }
+    },
+    jwt: ({ token, user }) => {
+      console.log('JWT Callback', { token, user })
+      if (user) {
+        const u = user as unknown as any
+        return {
+          ...token,
+          id: u.id,
+          randomKey: u.randomKey
+        }
+      }
+      return token
+    }
+  }
+}
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
